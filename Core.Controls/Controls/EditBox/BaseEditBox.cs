0646cefca68b1da1aafa0cf22c5c9e4be64c7d51
LittleNetworkHack <45490363+LittleNetworkHack@users.noreply.github.com>
2020-09-14 12:47:06 +0200

    Update BaseEditBox.cs

--- Core.Controls/Controls/EditBox/BaseEditBox.cs
+++ Core.Controls/Controls/EditBox/BaseEditBox.cs
@@ -13,380 +13,274 @@ using Core.Reflection;
 namespace Core.Controls
 {
 	[Designer("System.Windows.Forms.Design.TextBoxDesigner, System.Design, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")]
-	public abstract class BaseEditBox<TValue> : TextBox, ICoreControl
-    {
-        #region Internal
+	public abstract class BaseEditBox<TValue> : TextBox
+	{
+		#region Properties
 
-        public class EditBoxStateManager : StateComponent<TextBox>
-        {
-            public string Text { get; set; }
-            public int SelectionStart { get; set; }
-            public int SelectionLength { get; set; }
+		#region Fields
 
-            public EditBoxStateManager()
-            {
+		private bool _isValid = true;
+		private bool _isMandatory = false;
 
-            }
+		private TValue _value;
 
-            public override void SetState(TextBox box)
-            {
-                Text = box.Text;
-                SelectionStart = box.SelectionStart;
-                SelectionLength = box.SelectionLength;
-            }
+		#endregion Fields
 
-            public override void ApplyState(TextBox box)
-            {
-                box.Text = Text;
-                box.Select(SelectionStart, SelectionLength);
-            }
-        }
-
-        #endregion Internal
-
-        #region Private/Protected
-
-        private TValue _Value;
-        private Color _BorderColor = Color.Gray;
-
-        protected EditBoxStateManager BoxCache;
-
-        #endregion Private/Protected
-
-        #region Public Properties
-
-        [Browsable(false)]
-        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
-        public override string Text { get => base.Text; set => base.Text = value; }
-
-		public event EventHandler ValueChanged;
-        public event EventHandler ValidChanged;
-        
-        [DefaultValue(null)]
-        [Bindable(true)]
-        [Category(CoreDesignTime.C_Component)]
-        public TValue Value
-        {
-            get => _Value;
-            set
-			{
-                if (!SetValue(ref _Value, value, ValueChanged))
-                    return;
-
-                SetTextFromValue();
-            }
-        }
-
-        protected bool _IsValid = true;
-        [DefaultValue(true)]
-        [Category(CoreDesignTime.C_Component)]
-        public bool IsValid
-        {
-            get => _IsValid;
-            set
-            {
-                if (!SetValue(ref _IsValid, value, ValidChanged))
-                    return;
-
-                Invalidate();
-            }
-        }
-
-        protected bool _IsMandatory = false;
-        [DefaultValue(false)]
-        [Category(CoreDesignTime.C_Component)]
-        public bool IsMandatory
-        {
-            get => _IsMandatory;
-            set
-            {
-                if (!SetValue(ref _IsMandatory, value))
-                    return;
-
-                Invalidate();
-            }
-        }
-
-        #region Appearance
-
-        [DefaultValue(typeof(Font), "Microsoft Sans Serif, 8.25pt")]
-        [Category(CoreDesignTime.C_Appearance)]
-        public override Font Font
-        {
-            get => base.Font;
-            set => base.Font = value;
-        }
-
-        [DefaultValue(typeof(Color), "Black")]
-        [Category(CoreDesignTime.C_Appearance)]
-        public override Color ForeColor
-        {
-            get => base.ForeColor;
-            set => base.ForeColor = value;
-        }
-
-        [DefaultValue(typeof(Color), "White")]
-        [Category(CoreDesignTime.C_Appearance)]
-        public override Color BackColor
-        {
-            get => base.BackColor;
-            set => base.BackColor = value;
-        }
-
-        [DefaultValue(typeof(Color), "Gray")]
-        [Category(CoreDesignTime.C_Appearance)]
-        public Color BorderColor
-        {
-            get => _BorderColor;
-            set
-            {
-                if (!SetValue(ref _BorderColor, value))
-                    return;
-
-                Invalidate();
-            }
-        }
-
-        #endregion Appearance
-
-        #region Format
-
-        protected string _EditFormat;
-        [DefaultValue(null)]
-        [Category(CoreDesignTime.C_Appearance)]
-        public string EditFormat
-        {
-            get => _EditFormat;
-            set
-            {
-                if (!SetValue(ref _EditFormat, value))
-                    return;
-
-                SetTextFromValue();
-            }
-        }
-
-        protected string _DisplayFormat;
-        [DefaultValue(null)]
-        [Category(CoreDesignTime.C_Appearance)]
-        public string DisplayFormat
-        {
-            get => _DisplayFormat;
-            set
-            {
-                if (!SetValue(ref _DisplayFormat, value))
-                    return;
-
-                SetTextFromValue();
-            }
-        }
-
-        #endregion Format
-
-        #endregion Public Properties
-
-        #region Constructors
-
-        public BaseEditBox()
-        {
-			//ControlStyles style = ControlStyles.AllPaintingInWmPaint |
-			//                      ControlStyles.OptimizedDoubleBuffer |
-			//                      ControlStyles.ResizeRedraw |
-			//                      ControlStyles.UserPaint;
-			//SetStyle(style, true);
-			Font = new Font("Microsoft Sans Serif", 8.25F, GraphicsUnit.Point);
-			BackColor = Color.White;
-			ForeColor = Color.Black;
-            BoxCache = new EditBoxStateManager();
-            DebugInit();
-        }
-
-        #endregion Constructors
-
-        #region Component Model Pattern
-
-        protected bool SetValue<TField>(ref TField field, TField value, EventHandler handler = null, EventArgs args = null)
-        {
-            if (EqualityComparer<TField>.Default.Equals(field, value))
-                return false;
-
-            field = value;
-            args = args ?? EventArgs.Empty;
-            handler?.Invoke(this, args);
-
-            return true;
-        }
-
-        #endregion Component Model Pattern
-
-        #region Paint
-
-        protected ColorTriplet GetColors()
-        {
-            if (Enabled == false)
-                return CoreDesignTime.EditDisabled;
-            else if (ReadOnly)
-                return CoreDesignTime.EditReadOnly;
-            else if (Focused)
-                return CoreDesignTime.EditFocused;
-            else if (IsValid == false)
-                return CoreDesignTime.EditError;
-            else if (IsMandatory)
-                return CoreDesignTime.EditMandatory;
-            else
-                return CoreDesignTime.EditDefault;
-        }
-
-        #endregion Paint
-
-        #region Value
-
-        public abstract bool TryParsePartialValue(string text);
-        public abstract bool TryParseValue(string text, out TValue value);
-
-        #endregion Value
-
-        #region Input Logic
-
-        protected override void OnPreviewKeyDown(PreviewKeyDownEventArgs e)
-        {
-            BoxCache.SetState(this);
-            base.OnPreviewKeyDown(e);
-        }
-
-        protected override void OnTextChanged(EventArgs e)
-        {
-            if (BoxCache.Enter())
-                return;
-
-            if (TryParsePartialValue(Text))
-            {
-                BoxCache.SetState(this);
-                if (TryParseValue(Text, out TValue val))
-                    Value = val;
-                base.OnTextChanged(e);
-            }
-            else
-            {
-                BoxCache.ApplyState(this);
-            }
-
-            BoxCache.Leave();
-            Invalidate();
-        }
-
-        protected override void OnGotFocus(EventArgs e)
-        {
-            OnTextChanged(e);
-            SetTextFromValue();
-            Invalidate();
-            base.OnGotFocus(e);
-        }
-
-        protected override void OnLostFocus(EventArgs e)
-        {
-            if (TryParseValue(Text, out TValue val))
-                Value = val;
-
-            SetTextFromValue();
-            Invalidate();
-            base.OnLostFocus(e);
-        }
-
-        protected override void OnReadOnlyChanged(EventArgs e)
-        {
-            Invalidate();
-            base.OnReadOnlyChanged(e);
-        }
-
-        protected override void OnInvalidated(InvalidateEventArgs e)
-        {
-            ColorTriplet state = GetColors();
-
-            ForeColor = state.Foreground;
-            BackColor = state.Background;
-
-            base.OnInvalidated(e);
-        }
-
-        protected override void OnPaint(PaintEventArgs e)
-        {
-            base.OnPaint(e);
-            ControlPaint.DrawBorder(e.Graphics, ClientRectangle, BorderColor, ButtonBorderStyle.Solid);
-        }
-
-        #endregion Input Logic
-
-        #region Format Value
-
-        public void SetTextFromValue()
-        {
-            if (BoxCache.Enter())
-                return;
-
-            Text = FormatValue(Value);
-
-            BoxCache.Leave();
-        }
-
-        public virtual string FormatValue(TValue value)
-        {
-            if (value == null)
-                return string.Empty;
-            else if (value is IFormattable f)
-                return FormatValue(f, Focused);
-            return value.ToString();
-        }
-
-        public virtual string FormatValue(IFormattable value, bool focus)
-        {
-            if (focus)
-                return value.ToString(EditFormat, CultureInfo.CurrentCulture);
-            else
-                return value.ToString(DisplayFormat, CultureInfo.CurrentCulture);
-        }
-
-        #endregion Format Value
-
-        #region ICoreControl
-
-        object ICoreControl.Value
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+		public TValue Value
 		{
-            get => Value;
-            set => Value = (TValue)value;
-        }
-        Type ICoreControl.ValueType => typeof(TValue);
-        string ICoreControl.DataPath { get; set; }
-
-        [DefaultValue(null)]
-        public Label DescriptionLabel { get; set; }
-
-
-        public event EventHandler CoreSourceChanged;
-        private CoreDataSource _coreSource;
-        [DefaultValue(null)]
-        public CoreDataSource CoreSource
-		{
-            get => _coreSource;
-            set
-			{
-                if (ReferenceEquals(_coreSource, value))
-                    return;
-
-                _coreSource = value;
-                CoreSourceChanged.Invoke(this, EventArgs.Empty);
-            }
+			get => _value;
+			set => SetValue(ref _value, value);
 		}
 
-        //[TypeConverter(typeof(ExpandableObjectConverter))]
-        //[DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
-        //public CoreDataBinder ValueBind { get; private set; }
-
-        private void DebugInit()
+		[DefaultValue(true)]
+		public bool IsValid
 		{
-            //ValueBind = new CoreDataBinder<(this);
-        }
+			get => _isValid;
+			set => SetValue(ref _isValid, value, ForcePaint);
+		}
 
-        #endregion ICoreControl
-    }
+		[DefaultValue(false)]
+		public bool IsMandatory
+		{
+			get => _isMandatory;
+			set => SetValue(ref _isMandatory, value, ForcePaint);
+		}
+
+		#endregion Properties
+
+		#region Constructors
+
+		protected BaseEditBox()
+		{
+		}
+
+		#endregion Constructors
+
+		#region Core
+
+		const int WM_CUT = 0x0300;
+		const int WM_PASTE = 0x0302;
+
+		protected override void WndProc(ref Message m)
+		{
+			switch (m.Msg)
+			{
+				case WM_CUT:
+					WmCut(ref m);
+					break;
+				case WM_PASTE:
+					WmPaste(ref m);
+					break;
+				default:
+					base.WndProc(ref m);
+					break;
+			}
+		}
+
+		protected bool SetValue<T>(ref T field, T value, Action method = null)
+		{
+			if (EqualityComparer<T>.Default.Equals(field, value))
+				return false;
+
+			field = value;
+			method?.Invoke();
+			return true;
+		}
+
+		protected override void OnEnter(EventArgs e)
+		{
+			Text = FormatValue(Value, true);
+			ForcePaint();
+			base.OnEnter(e);
+		}
+
+		protected override void OnLeave(EventArgs e)
+		{
+			Text = FormatValue(Value, false);
+			ForcePaint();
+			base.OnLeave(e);
+		}
+
+		protected override void OnGotFocus(EventArgs e)
+		{
+			ForcePaint();
+			base.OnGotFocus(e);
+		}
+
+		protected override void OnLostFocus(EventArgs e)
+		{
+			if (IsMandatory)
+				IsValid = !IsValueEmtpy(Value);
+
+			IsValid = !(Value == null && IsMandatory);
+			ForcePaint();
+			base.OnLostFocus(e);
+		}
+
+		#endregion Core
+
+		#region Process Text
+
+		public abstract bool TryParsePartialValue(string text);
+		public abstract bool TryParseValue(string text, out TValue value);
+		public virtual bool IsValueEmtpy(TValue value) => value == null;
+
+		protected bool ProcessText(string input) => ProcessText(input, out _);
+		protected bool ProcessText(string input, out string previewText)
+		{
+			previewText = GetPreviewText(input);
+			bool pValid = TryParsePartialValue(previewText);
+			bool fValid = TryParseValue(previewText, out TValue v);
+			if (pValid && fValid)
+				Value = v;
+			else if (pValid)
+				Value = default(TValue);
+
+			return pValid;
+		}
+
+		protected string GetPreviewText(string input)
+		{
+			string current = base.Text;
+			int ss = SelectionStart;
+			int sl = SelectionLength;
+
+			if (input == "\b")
+			{
+				if (current.Length == 0)
+					return current;
+
+				ss = ss > 0 ? ss - 1 : 0;
+				sl = sl > 0 ? sl : 1;
+
+				current = current.Remove(ss, sl);
+				return current;
+			}
+			else if (ss != -1 && sl != 0)
+			{
+				current = current.Remove(ss, sl);
+			}
+
+			current = current.Insert(ss, input);
+			return current;
+		}
+
+		#region Overrides
+
+		[Browsable(false)]
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+		public override string Text
+		{
+			get => base.Text;
+			set => base.Text = value;
+		}
+
+		protected override void OnKeyPress(KeyPressEventArgs e)
+		{
+			if (!ProcessText(e.KeyChar.ToString()))
+				e.Handled = true;
+
+			base.OnKeyPress(e);
+		}
+
+		protected override void OnKeyDown(KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Delete)
+				e.Handled = true;
+
+			base.OnKeyDown(e);
+		}
+
+		protected virtual void WmPaste(ref Message m)
+		{
+			if (Clipboard.ContainsText() && ProcessText(Clipboard.GetText()))
+				base.WndProc(ref m);
+		}
+
+		protected virtual void WmCut(ref Message m)
+		{
+			if (ProcessText(string.Empty))
+				base.WndProc(ref m);
+		}
+
+		#endregion Overrides
+
+		#endregion Process Text
+
+		#region Format Text
+
+		private string _editFormat = null;
+		private string _displayFormat = null;
+
+		[DefaultValue(null)]
+		public virtual string EditFormat
+		{
+			get => _editFormat;
+			set => SetValue(ref _editFormat, value, Invalidate);
+		}
+
+		[DefaultValue(null)]
+		public virtual string DisplayFormat
+		{
+			get => _displayFormat;
+			set => SetValue(ref _displayFormat, value, Invalidate);
+		}
+
+		public virtual string FormatValue(TValue value, bool isEditing)
+		{
+			if (value == null)
+				return string.Empty;
+			else if (value is IFormattable f)
+				return FormatValue(f, isEditing);
+			return value.ToString();
+		}
+
+		public virtual string FormatValue(IFormattable value, bool isEditing)
+		{
+			if (isEditing)
+				return value.ToString(EditFormat, CultureInfo.CurrentCulture);
+			else
+				return value.ToString(DisplayFormat, CultureInfo.CurrentCulture);
+		}
+
+		#endregion Format Text
+
+		#region Paint
+
+		protected Color BorderColor => this.GetBorderColor(DesignMode);
+
+		[Browsable(false)]
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+		public override Color BackColor
+		{
+			get => this.GetBackgroundColor(DesignMode);
+			set => base.BackColor = value;
+		}
+
+		[Browsable(false)]
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+		public override Color ForeColor
+		{
+			get => this.GetForegroundColor(DesignMode);
+			set => base.ForeColor = value;
+		}
+
+		protected void ForcePaint()
+		{
+			BackColor = BackColor;
+			OnBackColorChanged(EventArgs.Empty);
+			ForeColor = ForeColor;
+			OnForeColorChanged(EventArgs.Empty);
+			Invalidate();
+		}
+
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			base.OnPaint(e);
+			ControlPaint.DrawBorder(e.Graphics, ClientRectangle, BorderColor, ButtonBorderStyle.Solid);
+		}
+
+		#endregion Paint
+	}
 }
